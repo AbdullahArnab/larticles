843f7b77f441828ccd08d26ef337ff494333cee0
arnab <abdullah.arnab@gmail.com>
2019-11-09 23:33:01 +0600

    initial commit

---
+++ resources/js/components/Articles.vue
@@ -0,0 +1,123 @@
+<template>
+    <div>
+        <h2>
+            Articles
+        </h2>
+        <form @submit.prevent="addArticle" class="mb-2">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Title" v-model="article.title">
+            </div>
+            <div class="form-group">
+                <textarea type="text" class="form-control" placeholder="Body" v-model="article.body"></textarea>
+            </div>
+            <button type="submit" class="btn btn-light">Save</button>
+        </form>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item">
+                    <a class="page-link" href="#" @click="fetchArticles(pagination.prev_page_url)">Previous</a>
+                </li>
+                <li class="page-item disabled">
+                    <a href="#" class="page-link text-dark">Page {{pagination.current_page}} of {{pagination.last_page}}</a>
+                </li>
+                <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item">
+                    <a class="page-link" href="#" @click="fetchArticles(pagination.next_page_url)">Next</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
+            <h3>{{article.title}}</h3>
+            <p>{{article.body}}</p>
+            <hr>
+            <button class="btn btn-danger" @click="deleteArticle(article.id)">Delete</button>
+            <button class="btn btn-warning" @click="editArticle(article)">Delete</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                articles: [],
+                article: {
+                    'id': '',
+                    'title': '',
+                    'body': ''
+                },
+                article_id: '',
+                pagination: {},
+                edit: false
+            }
+        },
+
+        created() {
+            this.fetchArticles();
+        },
+
+        methods: {
+            fetchArticles(page_url){
+                let vm = this;
+                page_url = page_url || 'api/articles'
+                fetch(page_url)
+                    .then(res => res.json())
+                    .then(res => {
+                        this.articles = res.data;
+                        vm.makePagination(res.meta, res.links)
+                    })
+                    .catch(err => console.log(err));
+            },
+            makePagination(meta, links) {
+                let pagination = {
+                    current_page: meta.current_page,
+                    last_page: meta.last_page,
+                    next_page_url: links.next,
+                    prev_page_url: links.prev
+
+                };
+
+                this.pagination = pagination;
+            },
+            deleteArticle(id) {
+                if(confirm('Are You Confirm to Delete?')) {
+                    fetch(`api/article/${id}`, {
+                        method: 'delete'
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            alert('Article Removed');
+                            this.fetchArticles();
+                        })
+
+                }
+            },
+            addArticle() {
+                if(this.edit === false){
+                    fetch('api/article', {
+                        method: 'post',
+                        body: JSON.stringify(this.article),
+                        headers: {
+                            'content-type': 'application/json'
+                        }
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            this.article.title = '';
+                            this.article.body = '';
+                            alert('Article Added');
+                            this.fetchArticles();
+
+                        })
+                }
+            },
+            editArticle() {
+
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Larticles</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
